4fca777ada397f4356e9733b73c02571161225f7
Aaron Heesakkers <aaronheesakkers@gmail.com>
2016-09-14 13:54:22 +0200

    Simplified store

--- addon/services/store.js
+++ addon/services/store.js
@@ -1,194 +1,50 @@
-/**
-  @module ember-jsonapi-resources
-  @submodule store
-**/
-
 import Ember from 'ember';
 import { pluralize } from 'ember-inflector';
 
-/**
-  Service for a JSON API endpoint a facade to your resource adapter
-
-  @class StoreService
-  @requires Ember.Inflector
-  @static
-*/
+/*
+ * Caching version of ember-jsonapi-resource store.
+ *
+ * The original store is just a facade for the service of given `type`, which is just a facade
+ * for the adapter. It offers only the ease of not having to get the service yourself, and no
+ * other functionality.
+ *
+ * This store relies heavily on the ResourceOperations mixin, expecting most operations to go
+ * through Resource instances (which will use respective service/adapter). It removes the
+ * "redundant" createResource, createRelationship, etc, methods and provides a very simple API
+ * to create, find and peek resources.
+ *
+ * create:    Create and cache (but don't persist) new Resource.
+ * find:      Query server for Resource(s) with options.
+ * peek(All): Find (all) Resource(s) of `type` in cache.
+ *
+ * That's it.
+ */
 export default Ember.Service.extend({
-
-  /**
-    Find resource(s) using an id or a using a query `{id: '', query: {}}`
-
-    @method find
-    @param {String} type the entity or resource name will be pluralized unless a `{singleton: true}` option is passed
-    @param {Object|String} options object or (string) id
-    @return {Promise}
-  */
   find(type, options) {
-    let service = this._service(type, options);
-    return service.find(options);
+    return this._service(type).find(options);
   },
-
-  /**
-    Access to the cached object
-
-    @method all
-    @param {String} type the entity or resource name will be pluralized
-    @return {Ember.Array}
-  */
-  all(type) {
+  peek(type, id) {
+    return this._service(type).cacheLookup(id);
+  },
+  peekAll(type) {
     let service = this._service(type);
+    // TODO: cacheLookup without id gets all. Don't touch internal cache object here.
     return (service.cache && service.cache.data) ? service.cache.data : Ember.A([]);
   },
 
-  /**
-    Create a new resource, sends a POST request
-
-    @method createResource
-    @param {String} type the entity or resource name will be pluralized
-    @param {Resource} resource instance to serialize
-    @return {Promise}
-  */
-  createResource(type, resource) {
-    let service = this._service(type);
-    return service.createResource(resource);
+  create(type, attributes) {
+    return this._modelFactory(type).create({attributes: attributes});
   },
 
-  /**
-    Patch an existing resource, sends a PATCH request.
-
-    @method updateResource
-    @param {String} type the entity or resource name will be pluralized
-    @param {Resource} resource instance to serialize the changed attributes
-    @return {Promise}
-  */
-  updateResource(type, resource) {
-    let service = this._service(type);
-    return service.updateResource(resource);
+  _modelFactory(type) {
+    return Ember.getOwner(this).lookup(`model:${type}`);
   },
-
-  /**
-    Delete an existing resource, sends a DELETE request
-
-    @method deleteResource
-    @param {String} type the entity or resource name will be pluralized
-    @param {String|Resource} resource name (plural) or instance with self link
-    @return {Promise}
-  */
-  deleteResource(type, resource) {
-    let service = this._service(type);
-    return service.deleteResource(resource);
-  },
-
-  /**
-    Create (add) a relationship for `to-many` relation, sends a POST request.
-
-    See: <http://jsonapi.org/format/#crud-updating-to-many-relationships>
-
-    Adds a relation using a payload with a resource identifier object:
-
-    ```
-    {
-      "data": [
-        { "type": "comments", "id": "12" }
-      ]
+  _service(type) {
+    const _type = pluralize(type);
+    if (!this[_type]) {
+      throw new Error(`${_type} service not initialized`);
     }
-    ```
 
-    @method createRelationship
-    @param {String} type the entity or resource name will be pluralized
-    @param {Resource} resource instance, has URLs via it's relationships property
-    @param {String} relationship name (plural) to find the url from the resource instance
-    @param {String} id of the related resource
-    @return {Promise}
-  */
-  createRelationship(type, resource, relationship, id) {
-    let service = this._service(type);
-    return service.createRelationship(resource, relationship, id);
-  },
-
-  /**
-    Patch a relationship, either adds or removes everyting, sends a PATCH request.
-
-    See: <http://jsonapi.org/format/#crud-updating-to-one-relationships>
-
-    For `to-one` relation:
-
-    - Remove (delete) with payload: `{ "data": null }`
-    - Create/Update with payload:
-      ```
-      {
-        "data": { "type": "comments", "id": "1" }
-      }
-      ```
-
-    For `to-many` relation:
-
-    - Remove (delete) all with payload: `{ "data": [] }`
-    - Replace all with payload:
-      ```
-      {
-        "data": [
-          { "type": "comments", "id": "1" },
-          { "type": "comments", "id": "2" }
-        ]
-      }
-      ```
-
-    @method patchRelationship
-    @param {String} type the entity or resource name will be pluralized
-    @param {Resource} resource instance, has URLs via it's relationships property
-    @param {String} relationship name (plural) to find the url from the resource instance
-    @return {Promise}
-  */
-  patchRelationship(type, resource, relationship) {
-    let service = this._service(type);
-    return service.patchRelationship(resource, relationship);
-  },
-
-  /**
-    Deletes a relationship for `to-many` relation, sends a DELETE request.
-
-    See: <http://jsonapi.org/format/#crud-updating-to-many-relationships>
-
-    Remove using a payload with the resource identifier object:
-
-    For `to-many`:
-
-    ```
-    {
-      "data": [
-        { "type": "comments", "id": "1" }
-      ]
-    }
-    ```
-
-    @method deleteRelationship
-    @param {String} type the entity or resource name will be pluralized
-    @param {Resource} resource instance, has URLs via it's relationships property
-    @param {String} relationship name (plural) to find the url from the resource instance
-    @param {String} id of the related resource
-    @return {Promise}
-  */
-  deleteRelationship(type, resource, relationship, id) {
-    let service = this._service(type);
-    return service.deleteRelationship(resource, relationship, id);
-  },
-
-  /**
-    Lookup the injected service for a resource, pluralize type arg.
-
-    @private
-    @method _service
-    @param {String} type - the entity or resource name will be pluralized unless a `{singleton: true}` option is passed
-    @param {Object} options (object)
-  */
-  _service(type, options = {}) {
-    if (!options.singleton) {
-      type = pluralize(type);
-    }
-    if (!this[type]) {
-      throw new Error(type + ' service not initialized');
-    }
-    return this[type];
+    return this[_type];
   }
 });
