437da66f3ee9850ebd9bc680a7ded2f7dfd9ba4c
Aaron Heesakkers <aaronheesakkers@gmail.com>
2016-10-04 14:02:58 +0200

    Adds isDirty flag

--- addon/mixins/simple-resource-operations.js
+++ addon/mixins/simple-resource-operations.js
@@ -29,6 +29,20 @@ export default Ember.Mixin.create({
   }).readOnly().volatile(),
 
   /**
+    Flag indicating changes. Combines changedAttributes, Relationships
+    and _deleteOnSave.
+    @property isDirty
+    @type Boolean
+   */
+  isDirty: Ember.computed({
+    get() {
+      return !!this.get('_deleteOnSave') ||
+             !!this.get('changedAttributes') ||
+             !!this.get('changedRelationships');
+    }
+  }).readOnly().volatile(),
+
+  /**
     The service object for the entity (adapter with cache and serializer)
 
     @property service
@@ -37,10 +51,30 @@ export default Ember.Mixin.create({
   */
   service: Ember.required,
 
+  /**
+   * Schedule deletion of this resource.
+   *
+   * By scheduling deletion instead of destroy() or delete() instantly we
+   * keep all relationships and can fully rollback a parent resource.
+   */
+  _deleteOnSave: false,
+  deleteOnSave() {
+    this.set('_deleteOnSave', true);
+  },
+
   save() {
-    return this.get('isNew') ?
-      this.get('service').createResource(this) :
-      this.get('service').updateResource(this, this.get('includeRelationships'));
+    if (this.get('_deleteOnSave')) {
+      return this.delete();
+    } else {
+      return this.get('isNew') ?
+        this.get('service').createResource(this) :
+        this.get('service').updateResource(this, this.get('includeRelationships'));
+    }
+  },
+
+  rollback() {
+    this.set('_deleteOnSave', false);
+    return this._super();
   },
 
   delete() {
